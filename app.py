91efd57b11b3fe91f3a0fcaad20a5538a1a5e518
Elias Martin <elias@sportsvisio.com>
2024-10-07 15:03:52 -0400

    fixed issues regarding expirations

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
 import httpx
+import asyncio
 import nest_asyncio
 nest_asyncio.apply()
 
 from schwab.auth import easy_client
-import asyncio
 from helpers import load_config, precompile_numba_functions, get_risk_free_rate
 
 # Constants and Global Variables
 config = {}
-risk_free_rate = 0.0
-div_yield = 0.0
-contract_type = ""
-chain_primary_key = ""
 client = None
+
+r = 0.0
+q = 0.0
+option_type = ""
+
+ticker = ""
+chain_primary_key = ""
 
 async def main():
     """
     Main function to initialize the bot.
     """
-    global client, risk_free_rate, div_yield, contract_type, chain_primary_key
+    global client, r, q, option_type, chain_primary_key, ticker
     
     precompile_numba_functions()
     config = load_config()
-    risk_free_rate = get_risk_free_rate(config["FRED_API_KEY"])
-    if risk_free_rate is None:
+    r = get_risk_free_rate(config["FRED_API_KEY"])
+    if r is None:
         return
+    ticker = config["TICKER"]
 
     try:
         client = easy_client(
@@ -39,16 +43,40 @@ async def main():
         return
     
     try:
-        respDiv = await client.get_quote(config["TICKER"])
+        resp = await client.get_option_expiration_chain(ticker)        
+        assert resp.status_code == httpx.codes.OK
+        expirations = resp.json()
+
+        if expirations is not None and expirations["expirationList"]:
+            expiration_dates_list = []
+
+            for expiration in expirations["expirationList"]:
+                expiration_dates_list.append(expiration["expirationDate"])
+                
+            print(expiration_dates_list)
+        else:
+            print("Validation Failed", f"Invalid ticker symbol: {ticker}. Please use a valid ticker.")
+            return
+    except Exception as e:
+        print("Validation Failed", f"An error occurred: {str(e)}")
+        return
+    
+    try:
+        respDiv = await client.get_quote(ticker)
         assert respDiv.status_code == httpx.codes.OK
         div = respDiv.json()
-        div_yield = float(div[config["TICKER"]]["fundamental"]["divYield"]) / 100
+
+        q = float(div[ticker]["fundamental"]["divYield"]) / 100
     except Exception as e:
         print(f"An unexpected error occurred in options stream: {e}")
+        return
 
     #option_date = datetime.strptime(self.selected_date, "%Y-%m-%d").date()
-    contract_type = client.Options.ContractType.CALL if config["OPTION_TYPE"] == "calls" else client.Options.ContractType.PUT
+    option_type = client.Options.ContractType.CALL if config["OPTION_TYPE"] == "calls" else client.Options.ContractType.PUT
     chain_primary_key = "callExpDateMap" if config["OPTION_TYPE"] == "calls" else "putExpDateMap"
+
+    print(q)
+    print(r)
 
     await asyncio.sleep(1)
 
