114f179880fa30995a54fbc2150c3971e5946473
hedge0 <eliasmemail@gmail.com>
2024-10-07 14:00:08 -0400

    added div yield

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import httpx
 import nest_asyncio
 nest_asyncio.apply()
 
@@ -8,13 +9,14 @@ from helpers import load_config, precompile_numba_functions, get_risk_free_rate
 # Constants and Global Variables
 config = {}
 risk_free_rate = 0.0
+div_yield = 0.0
 client = None
 
 async def main():
     """
     Main function to initialize the bot.
     """
-    global client, risk_free_rate
+    global client, risk_free_rate, div_yield
     
     precompile_numba_functions()
     config = load_config()
@@ -33,6 +35,18 @@ async def main():
     except Exception as e:
         print("Login Failed", f"An error occurred: {str(e)}")
         return
+    
+    try:
+        respDiv = await client.get_quote(config["TICKER"])
+        assert respDiv.status_code == httpx.codes.OK
+        div = respDiv.json()
+        div_yield = float(div[config["TICKER"]]["fundamental"]["divYield"]) / 100
+    except Exception as e:
+        print(f"An unexpected error occurred in options stream: {e}")
+
+    #option_date = datetime.strptime(self.selected_date, "%Y-%m-%d").date()
+    contract_type = client.Options.ContractType.CALL if config["OPTION_TYPE"] == "calls" else client.Options.ContractType.PUT
+    chain_primary_key = "callExpDateMap" if config["OPTION_TYPE"] == "calls" else "putExpDateMap"
 
     await asyncio.sleep(1)
 
